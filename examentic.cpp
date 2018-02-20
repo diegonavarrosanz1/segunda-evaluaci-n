c7f38be29860be9d26ebdacd1e8afd8760d153b3
diegonavarrosanz1 <33655475+diegonavarrosanz1@users.noreply.github.com>
2018-02-20 11:22:28 +0100

    Add files via upload

---
+++ examentic.cpp
@@ -0,0 +1,41 @@
+
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main(){
+  
+   struct persona{
+        string nombre;
+        string apellido
+        int nota;
+        char fila;
+        int columna;
+        int salir
+        string persona=0
+     }; 
+  
+   for(cont=0,cont<9,cont++){
+    persona persona1;
+    persona persona2;
+    persona persona3;
+    cout<<"como te llamas:  ";
+    cin>>persona[cont].nombre;
+    cout<<"cual es tu apellido;  ";
+    cin>>persona[cont].apellido;
+    cout<<"nota de clase:  ";
+    cin>>persona[cont].nota;
+    cout<<"cual es tu fila:  ";
+    cin>>persona[cont].fila;
+    cout<<"cual es tu columna:  ";
+    cin>>persona[cont].columna
+  }
+  cout<<("toca una tecla para salir plis: ");
+  cin>>salir;
+}
+     
+    
+    
+  
+  
+    
+}
